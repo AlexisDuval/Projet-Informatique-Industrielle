32606a696d2fceedcd7cbbef60ced794ea385715
unknown <duvalalexis991@yahoo.frm>
2018-05-19 23:06:07 +0200

    debut socket

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+
+#define CHECK(sts, msg) if((sts) == -1) { perror(msg); exit(-1); }
+
 
 int main()
 {
-    printf("Hello World mother fuckdzddzer\n");
-    return 0;
+
+   struct sockaddr_un server, client;
+
+
+   /* création de la socket */ 
+    
+   int sock 
+   sock = socket(AF_INET, SOCK_DGRAM,0);
+   CHECK(sock, "Erreur à l'initialisation de la socket");
+   
+   CHECK(bind(sock,(struct sockaddr *) &server,))
+
 }
